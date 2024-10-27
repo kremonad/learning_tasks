5f5005c5f5b013b7b6005d4863e67a86659463ac
Alexey Kolesov <a.a.kolesov@breezeventures.io>
2024-10-27 20:42:50 +0300

    fix curator comments

--- 4_Sprint/4!_delivery_service/delivery_service.py
+++ 4_Sprint/4!_delivery_service/delivery_service.py
@@ -2,11 +2,11 @@
 import sys
 
 
-def main(robots_weight: list, limit: int) -> int:
+def main(robots_weight: list[int], limit: int) -> int:
     platforms = 0
     sorted_robots = sorted(robots_weight)
     if sorted_robots[-1] > limit:
-        return f'Вес отдельного робота не может превышать limit.'
+        return "Вес отдельного робота не может превышать limit."
     for _ in range(len(sorted_robots)):
         if sorted_robots[-1] + sorted_robots[0] <= limit:
             platforms = platforms + len(sorted_robots) // 2 + len(sorted_robots) % 2
@@ -17,7 +17,7 @@ def main(robots_weight: list, limit: int) -> int:
     return platforms
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     robots_weight = [int(el) for el in sys.stdin.readline().rstrip().split()]
     limit = int(input())
     result = main(robots_weight, limit)
