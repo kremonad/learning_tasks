d01e5c121ae8f92fc77ce0a20a66eb02db388eb3
ripmandin <72794420+ripmandin@users.noreply.github.com>
2024-11-19 23:56:28 +0300

    Finished 5 Sprint

--- 5_Sprint/5_rashifrovka/decryptor.py
+++ 5_Sprint/5_rashifrovka/decryptor.py
@@ -1,21 +1,21 @@
-# номер успешной посылки 122630276
+# номер успешной посылки 125682067
 
 def decrypt(command: str) -> str:
     result = ''
-    digit = '0123456789'
-    modifier = ''
-    for i in range(len(command)):
-        if command[i] in digit:
-            modifier += command[i]
-        elif command[i] == '[':
-            start = i + 1
-            end = command.index(']',-1)
-            content = decrypt(command[start:end])
-            result += int(modifier) * content
-            modifier = ''
-            return result
+    modifier = 0
+    stack = []
+    for i in command:
+        if i.isdigit():
+            modifier = modifier * 10 + int(i)
+        elif i == '[':
+            stack.append([result, modifier])
+            result = ''
+            modifier = 0
+        elif i == ']':
+            prev_result, modifier_new = stack.pop()
+            result = prev_result + result * modifier_new
         else:
-            result += command[i]
+            result += i
     return result
 
 
@@ -23,10 +23,3 @@ if __name__ == "__main__":
     short_command = input()
     result = decrypt(short_command)
     print(result)
-
-
-# 3[a]2[bc]
-# aaabcbc
-
-# 3[a2[c]]
-# accaccacc
