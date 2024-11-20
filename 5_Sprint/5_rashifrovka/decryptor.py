0a89d3719bb5519612e416cb777a40b93def6d34
Alexey Kolesov <a.a.kolesov@breezeventures.io>
2024-11-20 23:27:05 +0300

    Add fixes

--- 5_Sprint/5_rashifrovka/decryptor.py
+++ 5_Sprint/5_rashifrovka/decryptor.py
@@ -1,21 +1,24 @@
 # номер успешной посылки 125682067
 
+
 def decrypt(command: str) -> str:
-    result = ''
+    """Рекурсивная функция, которая преобразовывает шифр в команды"""
+    result = ""
+    digits = "0123456789"
     modifier = 0
     stack = []
-    for i in command:
-        if i.isdigit():
-            modifier = modifier * 10 + int(i)
-        elif i == '[':
-            stack.append([result, modifier])
-            result = ''
+    for symbol in command:
+        if symbol in digits:
+            modifier = modifier * 10 + int(symbol)
+        elif symbol == "[":
+            stack.append((result, modifier))
+            result = ""
             modifier = 0
-        elif i == ']':
+        elif symbol == "]":
             prev_result, modifier_new = stack.pop()
             result = prev_result + result * modifier_new
         else:
-            result += i
+            result += symbol
     return result
 
 
